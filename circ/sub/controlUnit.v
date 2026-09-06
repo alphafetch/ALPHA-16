99a1dfe6c3b705580f214fd38198de76e8689ebb
theTacocat88 <eliasglick1@gmail.com>
2026-09-06 13:50:54 -0400

    fix(controlUnit): ensure PC_HOLD is reset correctly in always block

--- circ/sub/controlUnit.v
+++ circ/sub/controlUnit.v
@@ -49,7 +49,8 @@ module controlUnit(
         IMMEDIATE = 16'h0000;
         CARRY_UPD = 1'b0;
         ALU_CIN_SEL = 2'b00;
-        PC_HOLD = 1'b0;
+
+        if (PC_HOLD != 1'b1) PC_HOLD = 1'b0;
 
         case (OPCODE)
             4'h0: begin // 0000 R-type
