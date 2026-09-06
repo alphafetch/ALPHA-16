b3d2cd1736f360a26b993402e285d78027c91e0b
theTacocat88 <eliasglick1@gmail.com>
2026-09-06 08:52:41 -0400

    fix(controlUnit): remove HALT opcode from invalid default case

--- sub/controlUnit.v
+++ sub/controlUnit.v
@@ -128,7 +128,7 @@ module controlUnit(
             4'hA: begin // 1010 HALT
                 PC_HOLD = 1'b1;
             end
-            default: ; // 1001, 1010, 1011, 1100, 1101, 1110, 1111 INVALID
+            default: ; // 1011, 1100, 1101, 1110, 1111 INVALID
         endcase
     end
 endmodule
